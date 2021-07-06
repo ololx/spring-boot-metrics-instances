5d00219374150d5fa0470f0110884c1c7595b731
Alexander A. Kropotin <ololx@icloud.com>
2021-07-06 17:24:16 +0500

    feature(reporitory.*) add repository for managing some data entities

---
+++ src/main/java/org/some/api/repository/SomeDataRepository.java
@@ -0,0 +1,15 @@
+package org.some.api.repository;
+
+import org.some.api.model.SomeData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @project some-api
+ * @created 2021-07-06 17:23
+ * <p>
+ * @author Alexander A. Kropotin
+ */
+@Repository
+public interface SomeDataRepository extends JpaRepository<SomeData, Long> {
+}
